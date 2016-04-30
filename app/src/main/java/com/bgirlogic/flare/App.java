b319a4522ad8e8c38481c5b73df55ab89dce87d3
Suhyun Kim <Suhyun.Kim@viacommix.com>
2016-04-30 15:40:39 -0400

    Added a custom Application class with Firebase context in it.

---
+++ app/src/main/java/com/bgirlogic/flare/App.java
@@ -0,0 +1,61 @@
+package com.bgirlogic.flare;
+
+import android.app.Application;
+import android.util.Log;
+
+import com.firebase.client.Firebase;
+
+/**
+ * Created by kimsuh on 4/25/16.
+ */
+public class App extends Application {
+
+    public static final String TAG = App.class.getSimpleName();
+
+    public static int[] myarr = new int[4];
+
+    /**
+     * A flag to distinguish if the current activity is visible
+     */
+    private static boolean currentActivityVisible = true;
+
+    /**
+     * A flag to distinguish if the previous activity is visible
+     */
+    private static boolean previousActivityVisible;
+
+    //TODO: figure out what are the benefits of having the static instance of app
+    private static App sInstance;
+
+    public App() {
+        //TODO: figure out the purpose of having the line below "super()"
+        super();
+        sInstance = this;
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Firebase.setAndroidContext(this);
+    }
+
+    public static boolean isActivityFullLaunch() {
+        return currentActivityVisible;
+    }
+
+    public static void activityResumed() {
+        currentActivityVisible = true;
+    }
+
+    public static void activityPaused() {
+        currentActivityVisible = false;
+        int myarrSize = myarr.length;
+        int myarrMid = myarrSize/2;
+        int numberToSearch = 3;
+        if (myarrMid < numberToSearch) {
+
+        }
+    }
+
+
+}
