0b8bd9428cccf576511fe1db6434200881e31227
Suhyun Kim <Suhyun.Kim@viacommix.com>
2016-05-15 07:28:22 -0400

    deleted strings

--- app/src/main/java/com/bgirlogic/flare/utils/Constants.java
+++ app/src/main/java/com/bgirlogic/flare/utils/Constants.java
@@ -1,16 +1,8 @@
 package com.bgirlogic.flare.utils;
 
-import android.content.res.Resources;
-
-import com.bgirlogic.flare.R;
-
 /**
  * Created by kimsuh on 5/15/16.
  */
 public class Constants {
-    public static final String MUSE_BASE_URL = Resources.getSystem()
-            .getResourceName(R.string.muse_base_url);
-
-    public static final String MUSE_QUERY_PAGE = Resources.getSystem()
-            .getResourceName(R.string.muse_query_page);
+    public static final String MUSE_BASE_URL = "https://api-v2.themuse.com";
 }
