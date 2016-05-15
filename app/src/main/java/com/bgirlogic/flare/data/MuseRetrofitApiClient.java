26a15783a41846c2182f8b84a2552c5535c997f7
Suhyun Kim <Suhyun.Kim@viacommix.com>
2016-05-15 07:23:10 -0400

    Added strings

--- app/src/main/java/com/bgirlogic/flare/data/MuseRetrofitApiClient.java
+++ app/src/main/java/com/bgirlogic/flare/data/MuseRetrofitApiClient.java
@@ -1,6 +1,7 @@
 package com.bgirlogic.flare.data;
 
 import com.bgirlogic.flare.data.models.Response1;
+import com.bgirlogic.flare.utils.Constants;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -29,12 +30,14 @@ public class MuseRetrofitApiClient implements MuseRetrofitService {
                 .create();
 
         mRetrofit = new RestAdapter.Builder()
-                .setEndpoint("https://api-v2.themuse.com")
+                .setEndpoint(Constants.MUSE_BASE_URL)
                 .setClient(new OkClient(mClient))
                 .setConverter(new GsonConverter(gson))
                 .build();
     }
 
+    //NOTE: the following string "page" cannot be added to strings.
+    //Retrofit in this case only accepts direct string constants.
     @Override
     public Response1 getInitialResults(@Query("page") String page) {
         return mRetrofit.create(MuseRetrofitService.class).getInitialResults(page);
---
+++ app/src/main/java/com/bgirlogic/flare/utils/Constants.java
@@ -0,0 +1,16 @@
+package com.bgirlogic.flare.utils;
+
+import android.content.res.Resources;
+
+import com.bgirlogic.flare.R;
+
+/**
+ * Created by kimsuh on 5/15/16.
+ */
+public class Constants {
+    public static final String MUSE_BASE_URL = Resources.getSystem()
+            .getResourceName(R.string.muse_base_url);
+
+    public static final String MUSE_QUERY_PAGE = Resources.getSystem()
+            .getResourceName(R.string.muse_query_page);
+}
